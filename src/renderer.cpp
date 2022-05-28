1ce9c49a7b36b09e783625bf9d7809ddf962d4c6
MatheusMarchetti <50531965+MatheusMarchetti@users.noreply.github.com>
2022-05-28 11:57:30 -0300

    Update renderer.cpp

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -10,9 +10,10 @@ Game::Game()
     this->viewLoc = this->sh.getUniformLocation("view");
     this->projLoc = this->sh.getUniformLocation("projection");
 
-    this->blocks_model = glm::mat4(1.0f);
     this->view = glm::mat4(1.0f);
     this->projection = glm::mat4(1.0f);
+    
+    //Also, always initialize your variables to something reasonable. I didn't do it for scale and rotation, try it yourself!
 
     std::cout << "gg" << std::endl;
     for (int i = 0; i < 10; i++)
@@ -36,8 +37,11 @@ void Game::render_game()
     {
         Mesh* m = &this->meshes[i];
 
-        this->blocks_model = glm::translate(this->blocks_model, glm::vec3(0.0f, 0.0f, 0.0f));
-        glUniformMatrix4fv(this->modelLoc, 1, GL_FALSE, glm::value_ptr(this->blocks_model));
+        m.position = glm::vec3(i * 0.5f, 0.0f, 0.0f); // 8 might end up out of your screen, it's harder to debug. Change this value at your needs (try having y and z variation as well)
+        
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), m.position) * glm::rotate(glm::mat4(1.0f), rotation) * glm::scale(glm::mat4(1.0f), scale) // Assembling the model transformation matrix (It should be the job of the mesh, in reality). Also, I don't remember how I named the scale and rotation vectors, haha. Please make sure it's right
+        
+        glUniformMatrix4fv(this->modelLoc, 1, GL_FALSE, glm::value_ptr(transform)); // Submiting your transform to shader
         glBindVertexArray(m->VAO);
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
